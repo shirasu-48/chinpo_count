6033daac1db1ee97ac6a4fa4044abd999a3796c3
Chirimen <86394918+shirasu-48@users.noreply.github.com>
2022-09-28 12:41:56 +0900

    Update chinpo.sh

--- chinpo.sh
+++ chinpo.sh
@@ -1,2 +1,16 @@
 
-
+#!/bin/bash
+s=("お" "ち" "ん" "ぽ")
+c=""
+n=0
+while :
+do
+  i=$((RANDOM % 4))
+  echo -n "${s[i]}"
+  c="$i${c:0:3}"
+  let n++
+  if [[ "$c" == "3210" ]];then
+    echo "おぉぉおﾞおﾞ～っ！！イグゥウ！！イッグゥウウ！！ $n 回目で果てました..."
+    break
+  fi
+done
